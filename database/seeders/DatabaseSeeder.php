b188381feec758cf0c3f90103ee4c4726df701c9
Muhamad Helmi <muhamadkelmi@gmail.com>
2024-05-25 18:15:42 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,34 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // User::factory(10)->create();       
 
-        User::factory()->create([
-            'name' => 'Helmi',
-            'username' => 'helmi',
-            'email' => 'muhamadkelmi@gmail.com',
-            'password' => bcrypt('Kelmi!@#')
-        ]);
-
-        User::factory()->create([
-            'name' => 'Edwan Isrin',
-            'username' => 'edwan',
-            'email' => 'edwanisrin31@gmail.com',
-            'password' => bcrypt('bandung')
-        ]);
-
-        User::factory()->create([
-            'name' => 'Sunardi',
-            'username' => 'sunardi',
-            'email' => 'sunardi_cigadung@gmail.com',
-            'password' => bcrypt('bandung')
-        ]);
-
-        User::factory()->create([
-            'name' => 'Ana',
-            'username' => 'ana',
-            'email' => 'ana_cigadung@gmail.com',
-            'password' => bcrypt('bandung')
-        ]);
     }
 }
